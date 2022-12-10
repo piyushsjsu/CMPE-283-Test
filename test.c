77ba93ca0885e0da8e5d92aa31023c46bacad332
piyushsjsu <piyushganesh.mamidwar@sjsu.edu>
2022-12-10 12:15:46 -0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/types.h>
+
+// Referred from https://www.spinics.net/lists/linux-tip-commits/msg61284.html
+
+static inline void __cpuid(unsigned int *eax,
+ 			unsigned int *ebx,
+ 			unsigned int *ecx,
+ 			unsigned int *edx)
+{
+ 	asm volatile("cpuid": "=a" (*eax),"=b" (*ebx),"=c" (*ecx),"=d" (*edx): "0" (*eax), "1" (*ebx), "2" (*ecx), "3" (*edx));
+}
+
+int main(int argc, char **argv)
+{
+	unsigned int eax, ebx, ecx, edx;
+	unsigned long long cycle_time;
+
+	eax = 0x4FFFFFFC;
+	__cpuid(&eax, &ebx, &ecx, &edx);
+	printf("CPUID(0x4FFFFFFC), Total exits  : %u \n", eax);
+
+	eax = 0x4FFFFFFD;
+	__cpuid(&eax, &ebx, &ecx, &edx);
+	cycle_time = (unsigned long long) ebx << 32 | ecx;
+	printf("CPUID(0x4FFFFFFD), Total time in VMM : %llu cycles \n", cycle_time);
+}
